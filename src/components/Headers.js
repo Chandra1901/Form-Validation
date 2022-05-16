d94e054647b142e7858f316a4d110a4232179eb0
Chandu11215 <kalalshannu19@gamil.com>
2022-05-16 09:27:01 +0530

    form changes

---
+++ src/components/Headers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Form.css'
+
+const  Headers = () => {
+  return (
+    <div className='Headers'>
+        <h1 className='head'>SOC2 Regulation Details</h1>
+        <br />
+        <h4 className='head'>New SOC2 Regulation Details</h4>
+    </div>
+  )
+}
+
+export default Headers
